d6f921c362e69855bd40aa9bc3fcaf3c1f372dbb
Codemaico <devwithmii@gmail.com>
2024-10-23 15:59:38 +0200

    Installed tailwind and made some UI changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { BudgetDetailsComponent } from './pages/budget-details/budget-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'details/:id',
+        component: BudgetDetailsComponent
+    },
+    {
+        path:'create-account',
+        component: CreateAccountComponent
+    }
+];
