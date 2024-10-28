f0d05ff7f6019046f56a8656dea9b128943cc5b4
Codemaico <devwithmii@gmail.com>
2024-10-28 11:39:29 +0200

    User service creation

--- src/app/pages/create-account/create-account.component.ts
+++ src/app/pages/create-account/create-account.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.css'
 })
 export class CreateAccountComponent {
+
+  accountForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required])
+  });
+
+  createAccount(){
+
+    
+  }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  USER: string = 'user';
+  constructor(private router: Router) { }
+
+
+  addUser(name: string){
+
+  }
+
+  getUser(){
+
+  }
+
+  deleteUser(){
+
+  }
+
+  isLoggedin(){
+    
+  }
 }
